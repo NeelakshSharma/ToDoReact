2ba3d559545fb74142a74715f919997f551440ba
Neelaksh <neelakshsharma1997@gmail.com>
2020-01-18 00:23:01 +0530

    Task items checking and unchecking

--- src/App.js
+++ src/App.js
@@ -12,9 +12,19 @@ export default class App extends React.Component {
     this.setState({
       list1: this.state.list1.concat(childData)
     });
-    console.log("added");
+    // console.log("added");
   };
   callBackDoneFunction = childData => {
+    console.log("task done: " + childData);
+    for (var i = 0; i < this.state.list1.length; i++) {
+      if (this.state.list1[i].task === childData) {
+        let duplicate_list = this.state.list1;
+        duplicate_list.splice(i, 1);
+        this.setState({
+          list1: duplicate_list
+        });
+      }
+    }
     const taskDone = {
       task: childData,
       status: true
@@ -23,16 +33,40 @@ export default class App extends React.Component {
       list2: this.state.list2.concat(taskDone)
     });
   };
+  callBackUnDoneFunction = childData => {
+    console.log("task undone: " + childData);
+    for (var i = 0; i < this.state.list2.length; i++) {
+      if (this.state.list2[i].task === childData) {
+        let duplicate_list = this.state.list2;
+        duplicate_list.splice(i, 1);
+        this.setState({
+          list2: duplicate_list
+        });
+      }
+    }
+    const taskUndone = {
+      task: childData,
+      status: false
+    };
+    this.setState({
+      list1: this.state.list1.concat(taskUndone)
+    });
+  };
   render() {
     return (
       <div>
         {/* just for testing {this.state.list1.length} */}
         <AddTask parentCallBack={this.callBackPushFunction} />
-        <TaskList
-          list1={this.state.list1}
-          list2={this.state.list2}
-          parentCallBack={this.callBackDoneFunction}
-        />
+        {this.state.list1.length === 0 && this.state.list2.length === 0 ? (
+          <h3>Add something</h3>
+        ) : (
+          <TaskList
+            list1={this.state.list1}
+            list2={this.state.list2}
+            parentCallBack={this.callBackDoneFunction}
+            parentCallBack2={this.callBackUnDoneFunction}
+          />
+        )}
         {/* just for testing {this.state.list2.length} */}
       </div>
     );
--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -3,42 +3,74 @@ import React from "react";
 export default class TaskList extends React.Component {
   handleDone = event => {
     if (event.target.checked) {
-      console.log(event.target.name);
       //got the name of task as event.target.name
       this.props.parentCallBack(event.target.name);
     }
   };
+  handleNotDone = event => {
+    console.log(event.target.name);
+    this.props.parentCallBack2(event.target.name);
+  };
   render() {
+    const list1 = this.props.list1;
+    const list2 = this.props.list2;
+    const tasksPendingList = (
+      <ul>
+        {list1.map(taskItem => (
+          <li>
+            <label>
+              <input
+                type="checkbox"
+                name={taskItem.task}
+                onChange={this.handleDone}
+                checked={taskItem.status}
+              />
+              {taskItem.task}
+            </label>
+          </li>
+        ))}
+      </ul>
+    );
+    const tasksDoneList = (
+      <ul>
+        {list2.map(taskItem => (
+          <li>
+            <label>
+              <input
+                type="checkbox"
+                name={taskItem.task}
+                onChange={this.handleNotDone}
+                checked={taskItem.status}
+              />
+              {taskItem.task}
+            </label>
+          </li>
+        ))}
+      </ul>
+    );
+    if (list1.length !== 0) {
+      if (list2.length === 0) {
+        return (
+          <div>
+            <h3>Finish these tasks and continue adding more.</h3>
+            {tasksPendingList}
+          </div>
+        );
+      } else {
+        return (
+          <div>
+            <h3>Tasks Pending</h3>
+            {tasksPendingList}
+            <h3>Tasks completed</h3>
+            {tasksDoneList}
+          </div>
+        );
+      }
+    }
     return (
       <div>
-        <ul>
-          {this.props.list1.map(taskItem => (
-            <li>
-              <label>
-                <input
-                  type="checkbox"
-                  name={taskItem.task}
-                  onChange={this.handleDone}
-                />
-                {taskItem.task}
-              </label>
-            </li>
-          ))}
-        </ul>
-        <ul>
-          {this.props.list2.map(taskItem => (
-            <li>
-              <label>
-                <input
-                  type="checkbox"
-                  name={taskItem.task}
-                  onChange={this.handleNotDone}
-                />
-                {taskItem.task}
-              </label>
-            </li>
-          ))}
-        </ul>
+        <h2>You're good to go.</h2>
+        {tasksDoneList}
       </div>
     );
   }
